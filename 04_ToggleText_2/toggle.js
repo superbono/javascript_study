1d011758d4d7655841e73eea8a85635371e33c4d
superbono <choiar@pincar.co.kr>
2022-06-07 02:21:16 +0900

    토글(클릭 시 색상변경+토글+텍스트변경)_04

--- 04_ToggleText_2/toggle.js
+++ 04_ToggleText_2/toggle.js
@@ -1,6 +1,29 @@
 window.onload = () => {
 
+    let title = document.querySelector(".title");
+    let textShow = document.querySelector(".textShow");
+    let colorChange = document.querySelector(".colorChange");
 
+    textShow.addEventListener("click", () => {
+        title.classList.toggle("show");
+        let titleToggle = document.querySelector('.show');
+        if (titleToggle) {
+            textShow.innerHTML = "텍스트 숨기기";
+        } else {
+            textShow.innerHTML = "텍스트 노출";
+        }
+    });
+    colorChange.addEventListener("click", () => {
+        colorChange.classList.toggle("change");
+        let change = document.querySelector('.change');
+        if (change) {
+            title.style.color = 'tomato';
+            colorChange.innerHTML = "색상 리셋";
+        } else {
+            title.style.color = '#000';
+            colorChange.innerHTML = "색상 변경";
+        }
+    });
 
 }
 
